c0a7c75148f84851f2485a9c44f547625e244fa5
jonnoliveira <jonnutri@gmail.com>
2023-08-14 19:19:29 -0300

    feat: test admin_controller - req09

--- tests/controllers/admin/test_admin_controller.py
+++ tests/controllers/admin/test_admin_controller.py
@@ -1,6 +1,39 @@
-# from src.models.history_model import HistoryModel
-# from src.models.user_model import UserModel
+from src.models.history_model import HistoryModel
+from src.models.user_model import UserModel
 
 
 def test_history_delete(app_test):
-    raise NotImplementedError
+    users = [
+        {"name": "Jonn", "level": "admin", "token": "1234"},
+    ]
+    historys = [
+        {
+            "text-to-translate": "Do you love music?",
+            "translate-from": "en",
+            "translate-to": "pt",
+        },
+        {
+            "text-to-translate": "Do you love Elvis?",
+            "translate-from": "en",
+            "translate-to": "pt",
+        },
+    ]
+
+    for user in users:
+        UserModel(user).save()
+
+    for history in historys:
+        HistoryModel(history).save()
+
+    user = UserModel.find_one({"name": "Jonn"})
+    history = HistoryModel.find_one(
+        {"text-to-translate": "Do you love Elvis?"}
+    )
+
+    app_test.delete(
+        f"/admin/history/{history.data['_id']}",
+        headers={
+            "Authorization": user.data["token"],
+            "User": user.data["name"],
+        },
+    )
