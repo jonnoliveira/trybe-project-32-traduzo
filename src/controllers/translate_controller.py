d09bc32ab309fdf6c0bc2c9d1002d1cd524796f9
jonnoliveira <jonnutri@gmail.com>
2023-08-14 17:00:28 -0300

    feat: reverse translate implementation - req06

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -38,4 +38,20 @@ def index():
 # Req. 6
 @translate_controller.route("/reverse", methods=["POST"])
 def reverse():
-    raise NotImplementedError
+    languages = LanguageModel.list_dicts()
+
+    text_to_translate = request.form["text-to-translate"]
+    translate_from = request.form["translate-from"]
+    translate_to = request.form["translate-to"]
+    traslated = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=traslated,
+    )
