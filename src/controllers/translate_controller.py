570cd8c04c4a67c634cf7ecbdfddf6ea40eba283
jonnoliveira <jonnutri@gmail.com>
2023-08-14 16:51:59 -0300

    feat: index translated implementation - req05

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, request
-
-# from deep_translator import GoogleTranslator
+from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
 
 # from models.history_model import HistoryModel
@@ -18,6 +17,22 @@ def index():
         return render_template(
             "index.html", languages=languages, translated="Tradução"
         )
+
+    text_to_translate = request.form["text-to-translate"]
+    translate_from = request.form["translate-from"]
+    translate_to = request.form["translate-to"]
+    traslated = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=traslated,
+    )
 
 
 # Req. 6
